ca56170acfface8ee0d8ff40f1a8f0f811399c79
Robert <rkty13@gmail.com>
2015-05-09 20:56:50 -0400

    first commit

---
+++ personalsite.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+var app = express();
+
+const port = process.env.PORT || 8080;
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+})
+
+app.use(express.static('public'));
+
+app.listen(port, function() {
+    console.log("Server started on port " + port + ".");
+});
---
+++ public/js/script.js
@@ -0,0 +1,39 @@
+(function($) {
+    $(document).ready(function() {
+        $('a#nav-link').click(function(event) {
+            event.preventDefault();
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            $('a#nav-link').removeClass('active');
+            $(this).addClass('active');
+            $('html, body').stop().animate({
+                scrollTop: dest
+            }, 500, 'easeInOutQuint');
+        });
+        $('.hover-scale').mouseenter(function() {
+            $(this).transition({
+                scale: 1.1
+            }, {
+                queue: false
+            });
+        });
+        $('.hover-scale').mouseleave(function() {
+            $(this).transition({
+                scale: 1
+            }, {
+                queue: false
+            });
+        });
+        $('span#typed').typed({
+            strings: ["a programmer.", "a student.", "a hacker."],
+            typeSpeed: 40,
+            loop: true,
+            backSpeed: 20,
+            startDelay: 20
+        });
+    });
+})(jQuery)
